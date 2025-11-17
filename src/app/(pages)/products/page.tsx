7338a53edf1e9a82b31ea4920dc83a46857309a6
Omar-MK-20 <omar.mk.201@gmail.com>
2025-11-17 17:54:28 +0200

    refactor: Improve product filtering logic and enhance error handling in ProductPage component

--- src/app/(pages)/products/page.tsx
+++ src/app/(pages)/products/page.tsx
@@ -4,47 +4,93 @@ import Loading from '@/app/loading';
 import NotFound from '@/app/not-found';
 import ProductContainer from '@/components/product/ProductContainer';
 import { ProductI } from '@/interfaces';
+import { RequestError } from '@/services/error.service';
 import { productService } from '@/services/product.service';
 import { useSearchParams } from 'next/navigation';
 import { useEffect, useMemo, useState } from 'react';
+import Error from './error';
 
 function ProductPage()
 {
 
   const searchParams = useSearchParams();
-  const categoryId = searchParams.get("category[in]");
-  const brandId = searchParams.get("brand");
+  const categoryId = searchParams.getAll("category[in]");
+  const brandId = searchParams.getAll("brand");
 
   const [products, setProducts] = useState<ProductI[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<null | RequestError>(null);
 
 
   const filteredProducts = useMemo(() =>
   {
     return products.filter(product =>
     {
-      const matchesBrand = !brandId || product.brand._id === brandId;
-      const matchesCategory = !categoryId || product.category._id === categoryId;
-      return matchesBrand && matchesCategory;
+      console.log(brandId.length != 0);
+      console.log(categoryId.length != 0);
+      console.log(categoryId.length != 0 && brandId.length != 0);
+      if (categoryId.length != 0 && brandId.length != 0)
+      {
+        return true;
+      }
+      else
+      {
+        let matchCategory: boolean = false;
+        for (let i = 0; i < categoryId.length; i++)
+        {
+          if (product.category._id === categoryId[i])
+          {
+            matchCategory = true;
+            continue;
+          }
+          else
+          {
+            continue;
+          }
+        }
+
+        let matchBrand: boolean = false;
+        for (let i = 0; i < brandId.length; i++)
+        {
+          if (product.brand._id === brandId[i])
+          {
+            matchBrand = true;
+            continue;
+          }
+          else
+          {
+            continue;
+          }
+        }
+
+        return matchCategory || matchBrand;
+      }
     });
   }, [products, brandId, categoryId]);
+
+
+
+  console.log(filteredProducts);
+
+  // const matchesBrand = !brandId || product.brand._id === brandId;
+  // const matchesCategory = !categoryId || product.category._id === categoryId;
+  // return matchesBrand && matchesCategory;
+
 
 
   async function getProducts() 
   {
     setIsLoading(true);
-    try 
+    const res = await productService.getAll();
+    if (!(res instanceof RequestError))
     {
-      const res = await productService.getAll();
+      console.log(res);
       setProducts(res.data);
+      setIsLoading(false);
     }
-    catch (error) 
+    else
     {
-      console.error("error in the product page", error);
-      throw Error();
-    }
-    finally 
-    {
+      setError(res);
       setIsLoading(false);
     }
   }
@@ -59,14 +105,21 @@ function ProductPage()
 
   return (
     <div className='pt-10'>
-      {isLoading ? (
-        <Loading />
-      ) : !products.length ? (
-        <NotFound />
-      ) : (
-        // Choose one of the transition containers above
-        <ProductContainer products={filteredProducts} />
-      )}
+      {isLoading ?
+        (<Loading />)
+        :
+        error
+          ?
+          <Error error={error} reset={window.history.back} />
+          :
+          !products.length
+            ?
+            (<NotFound />)
+            :
+            (
+              // Choose one of the transition containers above
+              <ProductContainer products={filteredProducts} />
+            )}
     </div>
   );
 }
