80cfcf671c0a28a0307c40d438546344efef5949
Omar-MK-20 <omar.mk.201@gmail.com>
2025-11-18 11:08:41 +0200

    refactor: Enhance error handling in BrandsPage and CategoriesPage components

--- src/app/(pages)/brands/page.tsx
+++ src/app/(pages)/brands/page.tsx
@@ -1,12 +1,22 @@
+import Error from '@/app/error';
 import Container from '@/components/category&brand/Container';
 import { BrandI } from '@/interfaces';
 import { brandService } from '@/services/brand.service';
+import { RequestError } from '@/services/error.service';
 
 async function BrandsPage()
 {
+  let brands: BrandI[];
 
-  const brands: BrandI[] = (await brandService.getAll()).data;
-
+  const res = await brandService.getAll();
+  if (res instanceof RequestError)
+  {
+    return <Error error={res} reset={window.history.back} />;
+  }
+  else
+  {
+    brands = res.data;
+  }
 
   return (
     <div className='pt-15'>
--- src/app/(pages)/categories/page.tsx
+++ src/app/(pages)/categories/page.tsx
@@ -1,19 +1,30 @@
+import Error from '@/app/error';
 import Container from '@/components/category&brand/Container';
 import { CategoryI } from '@/interfaces';
 import { categoriesServices } from '@/services/categories.service';
+import { RequestError } from '@/services/error.service';
 
-async function CategoriesPage() {
+async function CategoriesPage()
+{
+  let categories: CategoryI[];
 
-  const categories: CategoryI[] = (await categoriesServices.getAll()).data
-
+  const res = await categoriesServices.getAll();
+  if (res instanceof RequestError)
+  {
+    return <Error error={res} reset={window.history.back} />;
+  }
+  else 
+  {
+    categories = res.data;
+  }
 
   return (
     <div className='pt-15'>
 
-      <Container list={categories} name={"Categories"}/>
+      <Container list={categories} name={"Categories"} />
 
     </div>
-  )
+  );
 }
 
-export default CategoriesPage
+export default CategoriesPage;
