57088ac86b7e39e19c129af495fa766027523bce
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2026-01-10 16:32:57 +0200

    feat: Implement NextAuth authentication with credentials provider

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,52 @@
+import { authService } from "@/services/auth.service";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+
+const handler = NextAuth({
+    providers: [
+
+        CredentialsProvider({
+            // The name to display on the sign in form (e.g. "Sign in with...")
+            name: "Credentials",
+            // `credentials` is used to generate a form on the sign in page.
+            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+            // e.g. domain, username, password, 2FA token, etc.
+            // You can pass any HTML attribute to the <input> tag through the object.
+            credentials: {
+                email: { label: "Email", type: "text", placeholder: "email@example.com", autocomplete: "email" },
+                password: { label: "Password", type: "password", placeholder: "********", autocomplete: "current-password" }
+            },
+            async authorize(credentials, req)
+            {
+
+                const email = credentials?.email ?? "";
+                const password = credentials?.password ?? "";
+
+                const response = await authService.signin({ email, password });
+                // console.log({ response });
+
+                if (response.message != "success")
+                {
+                    return null;
+                }
+
+                const user = {
+                    id: response.user.email,
+                    name: response.user.name,
+                    email: response.user.email,
+                    role: response.user.role,
+                    token: response.token
+                };
+
+                return user;
+            }
+        })
+
+    ],
+    pages: {
+        signIn: "/auth/login"
+    }
+});
+
+export { handler as GET, handler as POST };
