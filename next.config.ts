00716be58d40f3673934b21351a0686a26e33432
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2026-01-18 08:18:32 +0200

    refactor: Comment out allowedDevOrigins and experimental serverActions in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 
+  // allowedDevOrigins: ['192.168.8.126'],
+
+  // experimental: {
+  //   serverActions: {
+  //     allowedOrigins: ['192.168.8.126'],
+  //   },
+  // },
+
   async redirects() 
   {
     return [
