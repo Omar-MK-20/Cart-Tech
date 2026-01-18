2fb98677d8eb5351a5e734324c99232b487ad083
Omar-MK-20 <omar.mk.201@gmail.com>
2026-01-18 17:29:43 +0200

    feat: Wrap NavBar component with SessionProvider for authentication context

--- src/components/layout/navBar/NavBarContainer.tsx
+++ src/components/layout/navBar/NavBarContainer.tsx
@@ -1,18 +1,23 @@
+'use client';
 import { SidebarProvider } from '@/components/ui';
 import { brands, categories } from '@/server/navBar/navData';
 import { NavBar } from './NavBar';
+import { SessionProvider } from 'next-auth/react';
 
 
-function NavBarContainer() {
+function NavBarContainer()
+{
     return (
         <header>
             <SidebarProvider>
                 <header className="fixed w-full backdrop-blur-md shadow-sm z-100">
-                    <NavBar brands={brands} categories={categories} />
+                    <SessionProvider>
+                        <NavBar brands={brands} categories={categories} />
+                    </SessionProvider>
                 </header>
             </SidebarProvider>
         </header>
-    )
+    );
 }
 
 export { NavBarContainer };
