5b91b98ca358050de5ce5a010e2d1e1e587b4906
Omar-MK-20 <omar.mk.201@gmail.com>
2026-01-20 16:13:38 +0200

    fix: Uncomment header tags in NavBarContainer component for clarity

--- src/components/layout/navBar/NavBarContainer.tsx
+++ src/components/layout/navBar/NavBarContainer.tsx
@@ -8,15 +8,15 @@ import { SessionProvider } from 'next-auth/react';
 function NavBarContainer()
 {
     return (
-        <header>
-            <SidebarProvider>
-                <header className="fixed w-full backdrop-blur-md shadow-sm z-100">
-                    <SessionProvider>
-                        <NavBar brands={brands} categories={categories} />
-                    </SessionProvider>
-                </header>
-            </SidebarProvider>
-        </header>
+        // <header>
+        <SidebarProvider>
+            <header className="fixed w-full backdrop-blur-md shadow-sm z-100">
+                <SessionProvider>
+                    <NavBar brands={brands} categories={categories} />
+                </SessionProvider>
+            </header>
+        </SidebarProvider>
+        // </header>
     );
 }
 
